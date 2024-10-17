de1657da471505c53931abf518d8a295f2df8fcc
Richard Mylo Asante <richardmyloasante@gmail.com>
2024-10-17 11:10:00 +0000

    info: contract code convention

---
+++ contracts/Crowdfunding.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//imports
+
+contract CrowdFunding {
+
+    //type declarations
+
+
+    //state variables
+
+
+
+    //events
+
+    
+
+    //errors
+
+
+
+    //modifiers
+
+
+    //
+    constructor() {
+        // ...
+    }
+
+    receive() external payable {
+        // ...
+    }
+
+    fallback() external {
+        // ...
+    }
+
+    // External functions
+    
+
+    // External functions that are view
+    
+
+    // External functions that are pure
+    
+
+    // Public functions
+    
+
+    // Internal functions
+    
+
+    // Private functions
+
+
+}
